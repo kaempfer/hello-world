75923098aad717089ebbce90a252c2a8e07100ff
kaempfer <asmbrat@gmail.com>
2020-12-10 12:50:24 +0300

    new content

--- like_button.js
+++ like_button.js
@@ -3,23 +3,31 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+	  constructor(props) {
+		      super(props);
+		      this.state = { liked: false };
+		    }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+	  render() {
+		      if (this.state.liked) {
+			            return 'You liked comment number ' + this.props.commentID;
+			          }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+		      return e(
+			            'button',
+			            { onClick: () => this.setState({ liked: true }) },
+			            'Like'
+			          );
+		    }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+// document.querySelectorAll('.like_button_container')
+//   .forEach(domContainer => {
+//       // Read the comment ID from a data-* attribute.
+//           const commentID = parseInt(domContainer.dataset.commentid, 10);
+//               ReactDOM.render(
+//                     e(LikeButton, { commentID: commentID }),
+//                           domContainer
+//                               );
+//                                 });
